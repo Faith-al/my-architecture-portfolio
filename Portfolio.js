45eaca6d344ef18ff55bfb53c45d2b5c99507148
Faith-al <1084180@hr.nl>
2024-12-31 17:47:51 +0100

    Create Portfolio.js

---
+++ Portfolio.js
@@ -0,0 +1,11 @@
+
+    function translateToEnglish() {
+        document.getElementById('titel').innerText = "Welcome to my Portfolio website";
+        document.getElementById('inleiding').innerText = "This is an example of a website in Dutch.";
+    }
+
+    function translateToDutch() {
+        document.getElementById('titel').innerText = "Welkom op mijn Portfolio";
+        document.getElementById('inleiding').innerText = "Bouwkundig ontwerper met een passie voor architectuur en innovatie.";
+    }
+
