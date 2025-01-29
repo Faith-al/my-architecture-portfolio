0e06fed1d9ca5329338afa3955a9539e968b7e9e
Faith-al <1084180@hr.nl>
2025-01-29 09:24:35 +0100

    Update Portfolio.js

--- Portfolio.js
+++ Portfolio.js
@@ -1,11 +1,33 @@
 
     function translateToEnglish() {
         document.getElementById('titel').innerText = "Welcome to my Portfolio website";
-        document.getElementById('inleiding').innerText = "This is an example of a website in Dutch.";
+        document.getElementById('inleiding').innerText = "Architectural Engineering Student with a Passion for Architecture and Innovation";
+        document.getElementById('overh2').innerText = "About Me";
+        document.getElementById('overp').innerText = "I am an architectural engineering student specializing in architecture, with experience in AutoCAD, Revit, and Twinmotion. My passion lies in creative, sustainable and innovative designs.";
+        document.getElementById('levenh3').innerText = "Lifetime-Adaptable Living";
+        document.getElementById('overmp').innerText = "About me";
+        document.getElementById('bekijkp').innerText = "Look at my work";
+        document.getElementById('naam').innerText = "Name";
+        document.getElementById('bericht').innerText = "Message"; 
+        document.getElementById('verzenden').innerText = "Send";   
     }
 
     function translateToDutch() {
         document.getElementById('titel').innerText = "Welkom op mijn Portfolio";
-        document.getElementById('inleiding').innerText = "Bouwkundig ontwerper met een passie voor architectuur en innovatie.";
+        document.getElementById('inleiding').innerText = "Bouwkundig student met een passie voor architectuur en innovatie.";
+        document.getElementById('overh2').innerText = "Over Mij";
+        document.getElementById('overp').innerText = "Ik ben een bouwkundig student gespecialiseerd in architectuur, met ervaring in AutoCAD, Revit, en Twinmotion. Mijn passie ligt in een creatieve, innovatieve en duurzame ontwerpen te maken.";
+        document.getElementById('levenh3').innerText = "Levensloopbestendig wonen";
+        document.getElementById('overmp').innerText = "Over mij";
+        document.getElementById('bekijkp').innerText = "Bekijk mijn Werk";
+        document.getElementById('naam').innerText = "Naam";
+        document.getElementById('bericht').innerText = "Bericht"; 
+        document.getElementById('verzenden').innerText = "Verzenden";     
+        document.getElementById('').innerText = "";        
+        document.getElementById('').innerText = "";        
+        document.getElementById('').innerText = "";
+        
+       
+        
     }
 
