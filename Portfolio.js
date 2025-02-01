7ab0fe3e2c454ab437a535305269dd7d5052badd
Faith-al <1084180@hr.nl>
2025-02-01 01:38:32 +0100

    Update Portfolio.js

--- Portfolio.js
+++ Portfolio.js
@@ -1,9 +1,8 @@
-
-    function translateToEnglish() {
+ function translateToEnglish() {
         document.getElementById('titel').innerText = "Welcome to my Portfolio website";
         document.getElementById('inleiding').innerText = "Architectural Engineering Student with a Passion for Architecture and Innovation";
         document.getElementById('overh2').innerText = "About Me";
-        document.getElementById('overp').innerText = "I am an architectural engineering student specializing in architecture, with experience in AutoCAD, Revit, and Twinmotion. My passion lies in creative, sustainable and innovative designs.";
+        document.getElementById('overp').innerText = "My name is Faith Bundu and I am an architectural engineering student specializing in architecture, with experience in AutoCAD, Revit, and Twinmotion. My passion lies in making creative, sustainable and innovative designs.";
         document.getElementById('levenh3').innerText = "Lifetime-Adaptable Living";
         document.getElementById('overmp').innerText = "About me";
         document.getElementById('bekijkp').innerText = "Look at my work";
@@ -16,7 +15,7 @@
         document.getElementById('titel').innerText = "Welkom op mijn Portfolio";
         document.getElementById('inleiding').innerText = "Bouwkundig student met een passie voor architectuur en innovatie.";
         document.getElementById('overh2').innerText = "Over Mij";
-        document.getElementById('overp').innerText = "Ik ben een bouwkundig student gespecialiseerd in architectuur, met ervaring in AutoCAD, Revit, en Twinmotion. Mijn passie ligt in een creatieve, innovatieve en duurzame ontwerpen te maken.";
+        document.getElementById('overp').innerText = "Mijn naam is Faith Bundu en ik ben een bouwkundig student gespecialiseerd in architectuur, met ervaring in AutoCAD, Revit, en Twinmotion. Mijn passie ligt in het maken van creatieve, innovatieve en duurzame ontwerpen.";
         document.getElementById('levenh3').innerText = "Levensloopbestendig wonen";
         document.getElementById('overmp').innerText = "Over mij";
         document.getElementById('bekijkp').innerText = "Bekijk mijn Werk";
@@ -30,4 +29,5 @@
        
         
     }
+
 
