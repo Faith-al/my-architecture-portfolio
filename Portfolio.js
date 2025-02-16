ece134766ba36884447dfc0cda8e16356c079632
Faith-al <1084180@hr.nl>
2025-02-16 21:55:57 +0100

    Update Portfolio.js

--- Portfolio.js
+++ Portfolio.js
@@ -1,33 +1,40 @@
- function translateToEnglish() {
+function translateToEnglish() {
         document.getElementById('titel').innerText = "Welcome to my Portfolio website";
         document.getElementById('inleiding').innerText = "Architectural Engineering Student with a Passion for Architecture and Innovation";
         document.getElementById('overh2').innerText = "About Me";
-        document.getElementById('overp').innerText = "My name is Faith Bundu and I am an architectural engineering student specializing in architecture, with experience in AutoCAD, Revit, and Twinmotion. My passion lies in making creative, sustainable and innovative designs.";
+        document.getElementById('overp').innerText = "My name is Faith Bundu, and I am a second-year architectural engineering student at Rotterdam University of Applied Sciences. I specialize in architecture, with experience in AutoCAD, Revit, and Twinmotion. My passion lies in creating creative, innovative, and sustainable designs.";
         document.getElementById('levenh3').innerText = "Lifetime-Adaptable Living";
         document.getElementById('overmp').innerText = "About me";
         document.getElementById('bekijkp').innerText = "Look at my work";
         document.getElementById('naam').innerText = "Name";
         document.getElementById('bericht').innerText = "Message"; 
         document.getElementById('verzenden').innerText = "Send";   
+        document.getElementById('pakhuis').innerText = "Warehouse Home";
+        document.getElementById('circb').innerText = "Circular Building Block";
+        document.getElementById('thuis').innerText = "Home";
+        document.getElementById('cvlogoLink').href= "Portfolio.photo/CV-pages-2.pdf";
     }
 
     function translateToDutch() {
         document.getElementById('titel').innerText = "Welkom op mijn Portfolio";
         document.getElementById('inleiding').innerText = "Bouwkundig student met een passie voor architectuur en innovatie.";
         document.getElementById('overh2').innerText = "Over Mij";
-        document.getElementById('overp').innerText = "Mijn naam is Faith Bundu en ik ben een bouwkundig student gespecialiseerd in architectuur, met ervaring in AutoCAD, Revit, en Twinmotion. Mijn passie ligt in het maken van creatieve, innovatieve en duurzame ontwerpen.";
+        document.getElementById('overp').innerText = "Mijn naam is Faith Bundu en ik ben een tweedejaars bouwkundestudent aan de Hogeschool Rotterdam. Ik ben gespecialiseerd in architectuur, met ervaring in AutoCAD, Revit, en Twinmotion. Mijn passie ligt in het maken van creatieve, innovatieve en duurzame ontwerpen.";
         document.getElementById('levenh3').innerText = "Levensloopbestendig wonen";
         document.getElementById('overmp').innerText = "Over mij";
         document.getElementById('bekijkp').innerText = "Bekijk mijn Werk";
         document.getElementById('naam').innerText = "Naam";
         document.getElementById('bericht').innerText = "Bericht"; 
         document.getElementById('verzenden').innerText = "Verzenden";     
-        document.getElementById('').innerText = "";        
-        document.getElementById('').innerText = "";        
+        document.getElementById('pakhuis').innerText = "Pakhuis woning";        
+        document.getElementById('circb').innerText = "Circulaire bouwsteen";        
+        document.getElementById('thuis').innerText = "Thuis";
+        document.getElementById('cvlogoLink').href= "Portfolio.photo/CV-pages-1.pdf";
         document.getElementById('').innerText = "";
-        
+        document.getElementById('').innerText = "";
        
-        
     }
+
+
 
 
