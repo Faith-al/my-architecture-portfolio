214a1c0d683f48eee871d3d172e2ae43c00d5924
Faith-al <1084180@hr.nl>
2025-03-06 00:42:09 +0100

    Update Portfolio.js

--- Portfolio.js
+++ Portfolio.js
@@ -1,11 +1,10 @@
-function translateToEnglish() {
-        document.getElementById('titel').innerText = "Welcome to my Portfolio website";
-        document.getElementById('inleiding').innerText = "Architectural Engineering Student with a Passion for Architecture and Innovation";
+
+    function translateToEnglish() {
+        document.getElementById('titel').innerText = "Welcome to My \n Portfolio website";
         document.getElementById('overh2').innerText = "About Me";
         document.getElementById('overp').innerText = "My name is Faith Bundu, and I am a second-year architectural engineering student at Rotterdam University of Applied Sciences. I specialize in architecture, with experience in AutoCAD, Revit, and Twinmotion. My passion lies in creating creative, innovative, and sustainable designs.";
         document.getElementById('levenh3').innerText = "Lifetime-Adaptable Living";
         document.getElementById('overmp').innerText = "About me";
-        document.getElementById('bekijkp').innerText = "Look at my work";
         document.getElementById('naam').innerText = "Name";
         document.getElementById('bericht').innerText = "Message"; 
         document.getElementById('verzenden').innerText = "Send";   
@@ -16,13 +15,11 @@ function translateToEnglish() {
     }
 
     function translateToDutch() {
-        document.getElementById('titel').innerText = "Welkom op mijn Portfolio";
-        document.getElementById('inleiding').innerText = "Bouwkundig student met een passie voor architectuur en innovatie.";
+        document.getElementById('titel').innerText = "Welkom op Mijn Portfolio";
         document.getElementById('overh2').innerText = "Over Mij";
         document.getElementById('overp').innerText = "Mijn naam is Faith Bundu en ik ben een tweedejaars bouwkundestudent aan de Hogeschool Rotterdam. Ik ben gespecialiseerd in architectuur, met ervaring in AutoCAD, Revit, en Twinmotion. Mijn passie ligt in het maken van creatieve, innovatieve en duurzame ontwerpen.";
         document.getElementById('levenh3').innerText = "Levensloopbestendig wonen";
         document.getElementById('overmp').innerText = "Over mij";
-        document.getElementById('bekijkp').innerText = "Bekijk mijn Werk";
         document.getElementById('naam').innerText = "Naam";
         document.getElementById('bericht').innerText = "Bericht"; 
         document.getElementById('verzenden').innerText = "Verzenden";     
@@ -34,6 +31,7 @@ function translateToEnglish() {
         document.getElementById('').innerText = "";
        
     }
+
 
 
 
