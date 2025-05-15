0fdebea0930c81ab642a8e65cc462a47d5888450
Faith-al <1084180@hr.nl>
2025-05-15 13:20:21 +0200

    Update Portfolio.js

--- Portfolio.js
+++ Portfolio.js
@@ -1,8 +1,7 @@
-
-    function translateToEnglish() {
+  function translateToEnglish() {
         document.getElementById('titel').innerText = "Welcome to My \n Portfolio website";
         document.getElementById('overh2').innerText = "About Me";
-        document.getElementById('overp').innerText = "My name is Faith Bundu, and I am a second-year architectural engineering student at Rotterdam University of Applied Sciences. I specialize in architecture with experience in Adobe Suite, Revit, and Twinmotion. My passion lies in creating creative, innovative, and sustainable designs.";
+        document.getElementById('overp').innerText = "My name is Faith Bundu, and I am a second-year architectural engineering student at Rotterdam University of Applied Sciences. I specialize in architecture, with experience in Rhino, Grasshopper, Autodesk Forma, Revit, and Twinmotion. My passion lies in creating creative, innovative, and sustainable designs.";
         document.getElementById('levenh3').innerText = "Lifetime-Adaptable Living";
         document.getElementById('overmp').innerText = "About me";
         document.getElementById('naam').innerText = "Name";
@@ -11,13 +10,16 @@
         document.getElementById('pakhuis').innerText = "Warehouse Home";
         document.getElementById('circb').innerText = "Circular Building Block";
         document.getElementById('thuis').innerText = "Home";
-        document.getElementById('cvlogoLink').href= "Portfolio.photo/CV-pages-2.pdf";
-    }
+        document.getElementById('levenh4').innerText = "Lifetime-Adaptable Living";
+        document.getElementById('skipiste').innerText = "Mountain ski station";       
+        document.getElementById('circb2').innerText = "Circular Building Block"; 
+       
+}
 
     function translateToDutch() {
         document.getElementById('titel').innerText = "Welkom op Mijn Portfolio";
         document.getElementById('overh2').innerText = "Over Mij";
-        document.getElementById('overp').innerText = "Mijn naam is Faith Bundu en ik ben een tweedejaars bouwkundestudent aan de Hogeschool Rotterdam. Ik ben gespecialiseerd in architectuur met ervaring in Adobe Suite, Revit, en Twinmotion. Mijn passie ligt in het maken van creatieve, innovatieve en duurzame ontwerpen.";
+        document.getElementById('overp').innerText = "Mijn naam is Faith Bundu en ik ben een tweedejaars bouwkundestudent aan de Hogeschool Rotterdam. Ik ben gespecialiseerd in architectuur, met ervaring in Rhino, Grasshopper, Autodesk Forma, Revit, en Twinmotion. Mijn passie ligt in het maken van creatieve, innovatieve en duurzame ontwerpen.";
         document.getElementById('levenh3').innerText = "Levensloopbestendig wonen";
         document.getElementById('overmp').innerText = "Over mij";
         document.getElementById('naam').innerText = "Naam";
@@ -26,11 +28,13 @@
         document.getElementById('pakhuis').innerText = "Pakhuis woning";        
         document.getElementById('circb').innerText = "Circulaire bouwsteen";        
         document.getElementById('thuis').innerText = "Thuis";
-        document.getElementById('cvlogoLink').href= "Portfolio.photo/CV-pages-1.pdf";
-        document.getElementById('').innerText = "";
+        document.getElementById('levenh4').innerText = "Levensloopbestendig wonen";
+        document.getElementById('skipiste').innerText = "Tussenstation skipiste";   
+        document.getElementById('circb2').innerText = "Circulaire bouwsteen"; 
         document.getElementById('').innerText = "";
        
     }
+
 
 
 
